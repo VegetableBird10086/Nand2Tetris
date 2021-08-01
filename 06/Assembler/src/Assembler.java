3d3492d64540aa2f1763d49adf8c90611a9a9644
VegetableBird10086 <60803858+VegetableBird10086@users.noreply.github.com>
2021-08-01 16:15:08 +0800

    Add files via upload

---
+++ 06/Assembler/src/Assembler.java
@@ -0,0 +1,12 @@
+public class Assembler {
+    public static void main(String[] args) {
+        if (args.length < 1 || args.length > 1) {
+            System.out.println("Usage: Assembler Foo.asm");
+            return;
+        }
+        SymbolTable symTable = new SymbolTable();
+        Code code = new Code();
+        Parser parser = new Parser(args[0], symTable, code);
+        parser.compile();
+    }
+}
